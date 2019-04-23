1ab4e9d40386eeb5471b707cc0641aa215a06090
nnjyami <y@179.jp>
2019-04-24 04:02:39 +0900

    Add webfont
    - Add Typekit Setting
    - Test vertical typeface

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <section>
     <div>
-      <Logo :width="50" />
-      <h2 class="subtitle">iOS でもできる最高の素振り環境</h2>
+      <h1 class="subtitle">
+        iOS で Vue 環境を作るための旅に出たら<br />
+        JAMStack な登壇資料ができて<br />
+        平成最後の登壇をしている話
+      </h1>
       <div class="links">
         <a href="https://nuxtjs.org/" target="_blank" class="button--green"
           >Documentation <IconLink
@@ -14,12 +17,10 @@
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
 import IconLink from "~/components/IconLink.vue";
 
 export default {
   components: {
-    Logo,
     IconLink
   }
 };
@@ -31,14 +32,13 @@ export default {
 }
 
 .subtitle {
-  font-weight: 300;
   font-size: 1.4em;
   color: #2e495e;
-  padding-bottom: 15px;
+  padding-bottom: 0px;
   writing-mode: vertical-rl;
   font-family: heisei-mincho-stdn, serif;
   font-feature-settings: "palt";
-  letter-spacing: 0.8em;
+  letter-spacing: 0.12em;
 }
 
 .links {
